bcdbddaa4f7e60ebac060934bffe25974963f7fc
Josh Cohen <jacohen10@EMAIL.COM>
2015-11-19 16:08:29 -0500

    most recent code for code review

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   devise_for :users, controllers: { registrations: "registrations" }
-  root to: 'restaurants#index'
+  root to: 'welcome#index'
   resources :restaurants do
     resources :menus
     resources :meals
